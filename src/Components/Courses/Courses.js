e30e37da2a67e39b29d8f387bb5de8847fe2b816
Antu_Das <antuchandradasp9@gmail.com>
2022-10-28 10:23:22 -0700

    Aditional Changes

--- src/Components/Courses/Courses.js
+++ src/Components/Courses/Courses.js
@@ -24,9 +24,6 @@ const Courses = () => {
     return (
         <div className='flex flex-wrap flex-col md:flex-row lg:flex-row px-6 my-16 gap-6'>
             <aside style={{height : '370px'}} className='text-center w-20% bg-emerald-500 p-6 rounded-xl '>
-                {/* <div className=" my-10 ">
-                    <Link className="btn btn-success">Get Premium Access</Link>
-                </div> */}
                 <h1 className=' my-8 text-2xl text-zinc-700'>Our Courses Catagories</h1>
                 {
                     catagories.map(course => <div className='my-2 text-xl text-zinc-700 justify-start'><Link to={`/course/${course.id}`} key={course.id} course={course}>{course.name}</Link></div>)
@@ -45,7 +42,7 @@ const Courses = () => {
                                 </figure>
                                 <div className="card-body items-center ">
                                     <h2 className="card-title text-center text-2xl text-emerald-500">{course.title} Course</h2>
-                                    {/* <h4 className='text-xl text-teal-600'>Description</h4> */}
+                                    
                                     <div className="card-actions">
                                         <button className="btn btn-primary">purchase Course</button>
                                     </div>
